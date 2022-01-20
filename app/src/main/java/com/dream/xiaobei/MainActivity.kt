5e164f2fa9a9a28e12aaea63f96e00db716351a4
xianyou.li <xianyou.li@tcl.com>
2022-01-20 15:22:14 +0800

    first commit

---
+++ app/src/main/java/com/dream/xiaobei/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.dream.xiaobei
+
+import android.os.Bundle
+import com.dream.xiaobei.databinding.ActivityMainBinding
+import com.tcl.base.common.ui.BaseActivity
+
+/**
+ *@author tiaozi
+ *@date   2021/12/8
+ *description
+ */
+class MainActivity : BaseActivity<MainViewModel,ActivityMainBinding>(){
+    override fun initView(savedInstanceState: Bundle?) {
+
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun initDataOnResume() {
+
+    }
+}
---
+++ app/src/main/java/com/dream/xiaobei/MainViewModel.kt
@@ -0,0 +1,12 @@
+package com.dream.xiaobei
+
+import com.tcl.base.common.BaseViewModel
+
+/**
+ *@author tiaozi
+ *@date   2021/12/8
+ *description
+ */
+class MainViewModel : BaseViewModel() {
+
+}
